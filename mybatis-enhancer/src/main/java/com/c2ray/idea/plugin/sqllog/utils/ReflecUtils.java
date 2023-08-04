873a95147be29842033dd4ee3372c185b6c7724d
c2ray <wanbinghuo@gmail.com>
2023-08-04 14:52:57 +0800

    feat: simplify type name

--- mybatis-enhancer/src/main/java/com/c2ray/idea/plugin/sqllog/utils/ReflecUtils.java
+++ mybatis-enhancer/src/main/java/com/c2ray/idea/plugin/sqllog/utils/ReflecUtils.java
@@ -1,7 +1,11 @@
 package com.c2ray.idea.plugin.sqllog.utils;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.Arrays;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -10,11 +14,21 @@ import java.util.stream.Collectors;
  */
 public class ReflecUtils {
 
+    private static final String TYPE_EXPRESSION = "[a-z0-9.]*(?=\\.\\w+)";
+
+
     public static String getMethodStr(Method method) {
         String clzName = method.getDeclaringClass().getName();
         String methodName = method.getName();
-        String params = Arrays.stream(method.getParameterTypes()).map(Class::getSimpleName).collect(Collectors.joining(", "));
+        String params = Arrays.stream(method.getGenericParameterTypes())
+                .map(Type::getTypeName).map(ReflecUtils::simplifyTypeName)
+                .collect(Collectors.joining(", "));
         return String.format("%s#%s(%s)", clzName, methodName, params);
+    }
+
+    private static String simplifyTypeName(String typeName) {
+        return typeName.replaceAll(TYPE_EXPRESSION, "")
+                .replaceAll("\\.", "");
     }
 
 }
